f3cf485ab6942b08d5d9bd6b7a38a546c4dc227d
sep <maraffonskiy@gmail.com>
2020-06-18 21:02:16 +0300

    view create

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -4,33 +4,45 @@
 
     <!-- Bootstrap шаблон... -->
 
-    <div class="panel-body">
-        <!-- Отображение ошибок проверки ввода -->
-    @include('common.errors')
+   <a href="{{route('tasks.create')}}">
+       <i class="fa fa-plus"></i> Добавить задачу
+   </a>
 
-    <!-- Форма новой задачи -->
-        <form action="{{ url('tasks') }}" method="POST" class="form-horizontal">
-        {{ csrf_field() }}
-
-        <!-- Имя задачи -->
-            <div class="form-group">
-                <label for="task-name" class="col-sm-3 control-label">Задача</label>
-
-                <div class="col-sm-6">
-                    <input type="text" name="name" id="task-name" class="form-control">
-                </div>
+    <!-- Текущие задачи -->
+    @if (count($tasks) > 0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Текущая задача
             </div>
 
-            <!-- Кнопка добавления задачи -->
-            <div class="form-group">
-                <div class="col-sm-offset-3 col-sm-6">
-                    <button type="submit" class="btn btn-default">
-                        <i class="fa fa-plus"></i> Добавить задачу
-                    </button>
-                </div>
-            </div>
-        </form>
-    </div>
+            <div class="panel-body">
+                <table class="table table-striped task-table">
 
-    <!-- TODO: Текущие задачи -->
+                    <!-- Заголовок таблицы -->
+                    <thead>
+                    <tr>
+                    <th>Task</th>
+                    <th>&nbsp;</th>
+                    </tr>
+                    </thead>
+
+                    <!-- Тело таблицы -->
+                    <tbody>
+                    @foreach ($tasks as $task)
+                        <tr>
+                            <!-- Имя задачи -->
+                            <td class="table-text">
+                                <div>{{ $task->name }}</div>
+                            </td>
+
+                            <td>
+                                <!-- TODO: Кнопка Удалить -->
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
 @endsection
